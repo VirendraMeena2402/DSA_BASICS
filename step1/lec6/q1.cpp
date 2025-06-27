8225e8b26a263c7f43247532c1b0cdb5fa1303b0
Virendra_meena <virendramina88@gmail.com>
2025-06-27 15:42:37 +0530

    step1/lec6

---
+++ step1/lec6/q1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int mostFrequentElement(vector<int>& arr) {
+        unordered_map<int, int> freq;
+        for (int num : arr) {
+            freq[num]++;
+        }
+
+        int maxFreq = 0;
+        int result = INT_MAX;
+
+        for (auto& entry : freq) {
+            int num = entry.first;
+            int count = entry.second;
+            if (count > maxFreq || (count == maxFreq && num < result)) {
+                maxFreq = count;
+                result = num;
+            }
+        }
+
+        return result;
+    }
+};
