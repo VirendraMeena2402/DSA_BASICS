d6d653286eb7c67a9805b5227e1abaef8762ad35
Virendra_meena <virendramina88@gmail.com>
2025-07-31 21:40:00 +0530

    lec1

---
+++ step 16/lec1/q3.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        return max(robLinear(nums, 0, n - 2), robLinear(nums, 1, n - 1));
+    }
+
+private:
+    int robLinear(vector<int>& nums, int start, int end) {
+        int prev2 = 0, prev1 = 0;
+        for (int i = start; i <= end; i++) {
+            int curr = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
